3df98d4b81a33285717e380c554f2edffd764d67
sara.haliti@student.uni-pr.edu <sara.haliti@student.uni-pr.edu>
2021-01-12 13:56:04 +0100

    Modifikime ne game

--- js/quiz.js
+++ js/quiz.js
@@ -195,11 +195,11 @@ function startTimer(time){
 }
 
 function startTimerLine(time){
-    counterLine = setInterval(timer, 29);
+    counterLine = setInterval(timer, 38);
     function timer(){
         time += 1; //upgrading time value with 1
         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time >540){ //if time value is greater than 549
+        if(time >549){ //if time value is greater than 549
             clearInterval(counterLine); //clear counterLine
         }
     }
